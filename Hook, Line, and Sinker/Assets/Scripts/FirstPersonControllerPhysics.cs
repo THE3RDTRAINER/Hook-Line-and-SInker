da8efb63bca5c077efc64b430930123df39bf15b
Emma <epyzynski1@gmail.com>
2019-06-29 15:07:43 -0400

    Work on physics based movement, should be ready for testing

--- Hook, Line, and Sinker/Assets/Scripts/FirstPersonControllerPhysics.cs
+++ Hook, Line, and Sinker/Assets/Scripts/FirstPersonControllerPhysics.cs
@@ -9,6 +9,7 @@ public class FirstPersonControllerPhysics : MonoBehaviour
     [SerializeField] Rigidbody player;
     [SerializeField] Transform FPCamera;
     [SerializeField] float jumpHeight=5f;
+    [SerializeField] float speed = .5f;
 
     // Start is called before the first frame update
     void Start()
@@ -26,9 +27,30 @@ public class FirstPersonControllerPhysics : MonoBehaviour
 
     void Movement()
     {
+        //side to side movement 
+        if (Input.GetButton("Horizontal"))
+        {
+            Vector3 sidemovement =new Vector3 (1f, 0f, 0f);
+            player.MovePosition(transform.position + sidemovement*(speed*Input.GetAxisRaw("Horizontal")));
+           
+            //player.AddForce(speed * Input.GetAxisRaw("Horizontal"), 0, 0,  ForceMode.Acceleration);
+            //player.AddRelativeForce(Vector3.forward * speed * Input.GetAxisRaw("Horizontal"));
+        }
+
+        //forward/backward movement
+        if (Input.GetButton("Vertical"))
+        {
+            player.MovePosition(transform.position + transform.forward * (speed * Input.GetAxisRaw("Vertical")));
+
+            //player.AddForce(0,0, speed * Input.GetAxisRaw("Vertical"), ForceMode.Acceleration);
+
+        }
+
+        //Jump 
         if (Input.GetButtonDown("Jump"))
         {
             Debug.Log("I am jumping");
+            player.AddForce(0, jumpHeight, 0, ForceMode.Impulse);
             player.AddForce(0, jumpHeight, 0, ForceMode.Impulse);
         }
     }
