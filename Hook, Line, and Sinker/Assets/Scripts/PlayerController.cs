cbd55aab39626385109390021245c712e580af17
THE3RDTRAINER <philiptraineriii@verizon.net>
2019-07-23 23:02:45 -0400

    Hookshot, sorry for the late push

--- Hook, Line, and Sinker/Assets/Scripts/PlayerController.cs
+++ Hook, Line, and Sinker/Assets/Scripts/PlayerController.cs
@@ -16,12 +16,21 @@ public class PlayerController : MonoBehaviour
 
     [Header("Controls")]
     [SerializeField] private KeyCode ShootRope;
-    [Range(0,1)]
+    [Range(0, 1)]
     [SerializeField] private float amountToAddToRope = .5f;
     private List<GameObject> grabbableObjects = new List<GameObject>();
     //[SerializeField] private KeyCode LongenRope;
     //[SerializeField] private KeyCode ShortenRope;
     // Start is called before the first frame update
+
+    [Header("Hookshot Options")]
+    [SerializeField] private float hookshotDistance, hookSpeed;
+    [SerializeField] private LayerMask InitialHit, HookableObjects;
+
+    //Hookshot Variables
+    private bool isHooked;
+    private Vector3 oldPosition;
+    [SerializeField] private Transform hookedObject;
     void Start()
     {
         lineRen = GetComponent<LineRenderer>();
@@ -41,7 +50,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonUp("Fire1") || Input.GetKeyUp(ShootRope))
         {
             Debug.Log("FIRE");
-            if(connectedObject == null)
+            if (connectedObject == null)
             {
                 Grapple(Camera.position, Camera.forward, 20f, LayersToCollideWith);
             }
@@ -49,10 +58,10 @@ public class PlayerController : MonoBehaviour
             {
                 UnGrapple();
             }
-            
+
         }
         lineRen.SetPosition(0, Camera.position);
-        if(connectedObject != null)
+        if (connectedObject != null)
         {
             lineRen.SetPosition(1, connectedObject.transform.position);
         }
@@ -61,10 +70,30 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButton("ShortenRope"))
         {
             retractRope();
-        }else if (Input.GetButton("LongenRope"))
+        } else if (Input.GetButton("LongenRope"))
         {
             ExtendRope();
         }
+
+        //Hookshot controls
+        if (Input.GetButtonUp("Fire2"))
+        {
+            Debug.Log("Hookshot fired!");
+            Hookshot(Camera.position, Camera.forward);
+        }
+        if(hookedObject != null && isHooked)
+        {
+            if(Vector3.Distance(transform.position,hookedObject.position) < 1.0f)
+            {
+                hookedObject = null;
+                isHooked = false;
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, hookedObject.position, .05f);
+            }
+        }
+
     }
     private void FixedUpdate()
     {
@@ -78,7 +107,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void Grapple(Vector3 pos, Vector3 dir,float maxDistance, LayerMask layersToHit)
+    void Grapple(Vector3 pos, Vector3 dir, float maxDistance, LayerMask layersToHit)
     {
         RaycastHit hit;
         if (Physics.Raycast(pos, dir, out hit, maxDistance, layersToHit, QueryTriggerInteraction.Collide))
@@ -130,6 +159,24 @@ public class PlayerController : MonoBehaviour
         foreach (GameObject i in grabbableObjects)
         {
             Gizmos.DrawLine(Camera.position, i.transform.position);
+        }
+    }
+    private void Hookshot(Vector3 pos, Vector3 dir)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(pos, dir, out hit, hookshotDistance, InitialHit, QueryTriggerInteraction.Collide)){
+            //Start the hookshot
+            if (hit.collider.gameObject.layer == 8)
+            {
+                isHooked = true;
+                hookedObject = hit.collider.transform;
+                oldPosition = transform.position;
+            }
+            //Plays the denied SFX
+            else
+            {
+                Debug.Log("Hookshot didn't hit anything!");
+            }
         }
     }
 }
