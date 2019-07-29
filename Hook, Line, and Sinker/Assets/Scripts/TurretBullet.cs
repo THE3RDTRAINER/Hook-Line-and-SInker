788f9730615392d1599f22f54c090a744e6ed9bc
Philip Trainer <philiptraineriii@verizon.net>
2019-07-28 20:03:07 -0400

    Bullet Prefab

---
+++ Hook, Line, and Sinker/Assets/Scripts/TurretBullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TurretBullet : MonoBehaviour
+{
+    [SerializeField] int playerLayer;
+    [SerializeField] float forceToApplyToPlayer, speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.layer == playerLayer && collision.collider.gameObject.GetComponent<Rigidbody>())
+        {
+            collision.collider.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * forceToApplyToPlayer);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
