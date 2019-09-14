a7227b74b2b7eef30a5d0c511b3ed5a5f67401cf
Emma <epyzynski1@gmail.com>
2019-09-14 14:07:38 -0400

    Pre-fall fest commit (timer is working)

---
+++ Hook, Line, and Sinker/Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float startTime;
+    [SerializeField] float endTime;
+    [SerializeField] float completeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+            startTime = Time.time;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 10 )
+        {
+
+                endTime = Time.time;
+                completeTime = endTime - startTime; 
+            
+        }
+    }
+}
